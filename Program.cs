1ca0f5e0a1941aaa0ee410a9a1e00c3ecba6e7a9
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2024-11-09 14:17:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Process.Start("notepad.exe");
+        Process.Start("notepad.exe");
+        Process.Start("notepad.exe");
+    }
+}
